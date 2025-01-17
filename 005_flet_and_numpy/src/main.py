4b3e625fb72fa55a31330a6cf69a895a6c383002
Florian REY <florianrey.info@gmail.com>
2025-01-17 20:32:04 +0100

    try flet and numpy

--- 005_flet_and_numpy/src/main.py
+++ 005_flet_and_numpy/src/main.py
@@ -1,16 +1,17 @@
 import flet as ft
+import numpy as np
 
 
 def main(page: ft.Page):
     counter = ft.Text("0", size=50, data=0)
 
-    def increment_click(e):
-        counter.data += 1
+    def random_click(e):
+        counter.data = np.random.randint(100, size=5)
         counter.value = str(counter.data)
         counter.update()
 
     page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
+        icon=ft.Icons.SHUFFLE, on_click=random_click
     )
     page.add(
         ft.SafeArea(
