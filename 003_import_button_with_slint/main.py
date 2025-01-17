959c761cadb0ed8acf0a01362580b80030e89b7c
Florian REY <florianrey.info@gmail.com>
2025-01-17 15:29:00 +0100

    try other thing

---
+++ 003_import_button_with_slint/main.py
@@ -0,0 +1,11 @@
+import slint
+
+
+# slint.loader will look in `sys.path` for `app-window.slint`.
+class App(slint.loader.app_window.AppWindow):
+    pass
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
