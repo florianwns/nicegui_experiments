10f9277ba0c56e2cab7d687095d09d7010c9be8b
Florian REY <florianrey.info@gmail.com>
2025-01-17 15:50:07 +0100

    remove streamlit

--- 004_streamlit_hello_world/main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Mon application Streamlit")
-st.write("Bienvenue sur mon application web")
-
-if st.button("Cliquez-moi"):
-    st.write("Vous avez cliqué sur le bouton!")
