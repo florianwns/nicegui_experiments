4808d80f9052921e8343e4ac54d7a273569bc872
Florian REY <florianrey.info@gmail.com>
2025-01-24 17:31:47 +0100

    gol in progress

---
+++ 005_game_of_life/TODO.md
@@ -0,0 +1,25 @@
+- [x] Add a template based on https://halimb.github.io/gol/
+
+- [x] Make speed button works
+
+- [ ] Add toggle play/button
+
+- [ ] Add empty canvas for the grid or svg
+
+- [ ] implement core of the algorithm
+
+- [ ] _
+
+- [ ] _
+
+- [ ] _
+
+- [ ] _
+
+- [ ] _
+
+- [ ] _
+
+- [ ] _
+
+- [ ] _
--- 005_game_of_life/main.py
+++ 005_game_of_life/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """The Game of Life implemented with nicegui
 
-A remake of : https://playgameoflife.com/
+A remake of : https://halimb.github.io/gol/
 """
 
 from nicegui import ui
@@ -9,14 +9,43 @@ from nicegui import ui
 
 class GameOfLife:
     def __init__(self):
+        self._speed = 1
+
+    def build(self):
         ui.label('This page is defined in a class.')
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):
+        self._speed = min(4, max(0.25, value))
+
+    def decrease_speed(self, *args, **kwarg):
+        self.speed /= 2
+
+    def increase_speed(self, *args, **kwarg):
+        self.speed *= 2
+
+
+def ti_icon(name: str, color="gray-500", size="sm", *args, **kwargs):
+    return ui.button(icon=f"ti-{name}", color=color, *args, **kwargs) \
+        .props(f'padding="{size}" size="{size}"') \
+        .classes("text-white")
 
 
 @ui.page('/')
-def game_of_life_page():
-    with ui.header().classes(replace='flex items-center px-2') as header:
-        ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flat color=white')
-        ui.space()
+def home():
+    gol = GameOfLife()
+
+    ui.add_head_html(
+        '<link href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css" rel="stylesheet" />'
+    )
+    ui.colors(primary="white")
+    with ui.header().classes(
+            replace='text-black bg-white flex items-center px-2 shadow-2'
+    ) as header:
         ui.label("Conway’s Game of Life").classes('font-bold text-2xl')
         ui.space()
         with ui.tabs().props("inline-label") as tabs:
@@ -24,18 +53,30 @@ def game_of_life_page():
             ui.tab('Lexicon', icon="menu_book")
             ui.tab('Help', icon="info")
 
-    with ui.footer(value=False) as footer:
-        ui.label('Footer')
+    with ui.footer().classes(
+            "bg-white text-black flex items-center px-4 shadow-2"
+    ) as footer:
+        with ui.row().classes("items-center"):
+            ti_icon("star")
+            ui.label('Generation : 0')
+        ui.space()
+        with ui.row().classes("items-center"):
+            ti_icon("control-shuffle")
+            ti_icon("trash").classes("q-mr-xl")
+            ti_icon("control-pause")
+            ti_icon("control-play")
+            ti_icon("control-skip-forward").classes("q-mr-xl")
+            ti_icon("pencil")
+            ti_icon("eraser")
+        ui.space()
+        with ui.row().classes("items-center"):
+            ti_icon("minus", size="xs", on_click=gol.decrease_speed)
+            ui.label('Speed : 1x').bind_text_from(gol, "speed", lambda value: f"Speed : {value}x")
+            ti_icon("plus", size="xs", on_click=gol.increase_speed)
 
-    with ui.left_drawer().classes('bg-gray-50') as left_drawer:
-        ui.label('Side menu')
-
-    with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
-        ui.button(on_click=footer.toggle, icon='contact_support').props('fab')
-
-    with ui.tab_panels(tabs, value='Board').classes('w-full'):
+    with (ui.tab_panels(tabs, value='Board').classes('w-full')):
         with ui.tab_panel('Board'):
-            gol = GameOfLife()
+            gol.build()
         with ui.tab_panel('Lexicon'):
             ui.label('Lexicon')
         with ui.tab_panel('Help'):
